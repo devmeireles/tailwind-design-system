65c75dbdde4575dabf05c84d4483187c030de0d2
Gabriel Meireles <meireleswebdesign@gmail.com>
2024-06-08 19:42:30 -0300

    adding major updates to package structure

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,27 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { resolve } from "path";
+import dts from "vite-plugin-dts";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    dts({
+      exclude: [
+        "**/test/**",
+        "setupAfterEnv.ts",
+        "**/*.test.ts",
+        "**/*.test.tsx",
+        "**/*.stories.tsx",],
+    }),
+  ],
+  build: {
+    copyPublicDir: false,
+    lib: {
+      entry: resolve(__dirname, "src/main.ts"),
+      formats: ["es"],
+      name: "tailwind-design-system",
+    },
+  },
+});
